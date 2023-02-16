b5118922eb1d935699bfd0f8741c3de995064fae
hiteshoswal9840 <hiteshoswal9840@gmail.com>
2023-02-16 19:10:13 +0530

    variable assiangment 01 updated
    variable assiangment 01 updated

---
+++ 01_variable_assignment.js
@@ -0,0 +1,21 @@
+console.log("****** step 1 ******");
+var myFullName;
+console.log(myFullName);
+myFullName="Hitesh Oswal"
+console.log(myFullName);
+myFullName="Hitesh Chaganlal Oswal"
+console.log(myFullName);
+
+console.log("****** step 2 ******");
+var myLove="I love only js";
+console.log(myLove);
+
+console.log("****** step 3 ******");
+var firstName="Hitesh";
+var lastName="Oswal";
+var age="26";
+var  address="Pune";
+var pincode="411039";
+var gender="Male";
+var isMarried="single";
+console.log(firstName,lastName,age,address,pincode,gender,isMarried);
