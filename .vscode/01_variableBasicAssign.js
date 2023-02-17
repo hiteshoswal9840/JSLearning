2ca7694b76b40bd0861916ebedd989139b2dbc47
hiteshoswal9840 <hiteshoswal9840@gmail.com>
2023-02-17 19:46:12 +0530

    01_variableBasicAssign updated
    01_variableBasicAssign updated

---
+++ .vscode/01_variableBasicAssign.js
@@ -0,0 +1,22 @@
+console.log("******* 1 *******");
+var pin = 411039;
+console.log("PIN:",pin, "|" ,"Type Of Pin:", typeof pin);
+
+console.log("******* 2 *******");
+var address = "Bhosari";
+console.log("address:",address, "|" ,"Type Of address:", typeof address);
+
+console.log("******* 3 *******");
+var isMale = true;
+console.log("isMale:",isMale, "|" ,"Type Of isMale:", typeof isMale);
+
+console.log("********************************************");
+console.log("Diff Betwn null/undefined");
+console.log("******* null *******");
+
+var foo = null;
+console.log("foo is type of :", typeof foo );
+
+console.log("******* undefined *******");
+var bar;
+console.log("bar is type of :", typeof bar );
