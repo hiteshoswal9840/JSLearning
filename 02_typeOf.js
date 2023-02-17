a5e89a223e4803ee6ed2ed61388115e176d0d675
hiteshoswal9840 <hiteshoswal9840@gmail.com>
2023-02-17 20:22:24 +0530

    created and updated datatypes
    created and updated datatypes

---
+++ 02_typeOf.js
@@ -0,0 +1,34 @@
+// 1st Question
+
+console.log("**********step 1***********");
+console.log("---Before intialization---");
+var bankName;
+console.log("value of Bankname :",bankName,"\nDatatype Of Bankname:",typeof bankName);
+console.log("---After intialization---");
+bankName = "SBI BANK"
+console.log("value of Bankname :",bankName,"\nDatatype Of Bankname:",typeof bankName);
+
+// 2nd Question
+console.log("**********step 2***********");
+var marks = "90%"
+console.log("value of marks :",marks,"\nDatatype Of marks:",typeof marks);
+
+// 3rd Question
+console.log("**********step 3***********");
+console.log("---Before intialization---");
+var isWorking;
+console.log("value of isWorking :",isWorking);
+console.log("---After intialization---");
+isWorking = true;
+console.log("value of isWorking :",isWorking , "\nDatatype of isworking:",typeof isWorking);
+
+// 4th Question
+console.log("**********step 4***********");
+
+console.log("----- a ------");
+var totalCount = "Hundred And Seven";
+console.log("value of totalCount :",totalCount , "\nDatatype of totalCount:",typeof totalCount);
+
+console.log("----- b ------");
+totalCount = 107;
+console.log("value of totalCount :",totalCount , "\nDatatype of totalCount:",typeof totalCount);
