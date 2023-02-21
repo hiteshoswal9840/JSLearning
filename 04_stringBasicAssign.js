1ef1128f8d5bb27abb5cb8bb38abb5342b5a5bb8
hiteshoswal9840 <hiteshoswal9840@gmail.com>
2023-02-21 22:50:01 +0530

    stringBasicAssign updated
    stringBasicAssign updated

---
+++ 04_stringBasicAssign.js
@@ -0,0 +1,14 @@
+// Question no 1
+
+function stringBasic(){
+    console.log("*****step 1*****");
+    console.log("My dream company is : Google");
+    console.log("*****step 2*****");
+    var hobby1 = "Reading";
+    var hobby2 = "Swimming";
+    var hobby3 = "reading motivational books";
+    console.log("My three hobbies are:",hobby1,hobby2,hobby3);
+    console.log("*****step 3*****");
+    console.log("Sum Of Hobbies is:",hobby1.length+hobby2.length+hobby3.length);
+}
+stringBasic();
